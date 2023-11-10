717360f75d3e313a93b4c31f6659be2826c07992
Fojel <79102693+fojelio@users.noreply.github.com>
2023-11-09 21:42:45 -0300

    Add files via upload

---
+++ jugadores.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-11-2023 a las 20:33:03
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tp46`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugadores`
+--
+
+CREATE TABLE `jugadores` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `pos` varchar(1) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `estatura` decimal(10,0) NOT NULL,
+  `peso` int(11) NOT NULL,
+  `nacionalidad` varchar(20) NOT NULL,
+  `ap` int(11) NOT NULL,
+  `sub` int(11) NOT NULL,
+  `a` int(11) NOT NULL,
+  `ga` int(11) NOT NULL,
+  `atajadas` int(11) NOT NULL,
+  `asistencias` int(11) NOT NULL,
+  `faltascometidas` int(11) NOT NULL,
+  `faltassufridas` int(11) NOT NULL,
+  `ta` int(11) NOT NULL,
+  `tr` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ server.js
@@ -0,0 +1,138 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const fs = require('fs');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "",
+   database: "tp46"
+ });
+
+ db.connect(function(err) {
+   if (err) throw err;
+   console.log("Conectado a la base de datos!");
+
+   app.post('/datos', (req, res) => {
+      const { nombre, edad, estatura, nacionalidad, pos, peso } = req.body;
+
+
+
+      const sql = `INSERT INTO jugadores (nombre, edad, estatura, nacionalidad, pos, peso) VALUES (?, ?, ?, ?, ?, ?)`;
+      db.query(sql, [nombre, edad, estatura, nacionalidad, pos, peso], (err, result) => {
+      if (err) {
+         console.error("Error al insertar en la base de datos:", err);
+         res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+         console.log("Datos insertados en la base de datos:", result);
+         res.json({ success: true });
+      }
+   });
+});
+
+
+app.get('/jugadores-argentinos', (req, res) => {
+   const sql = `SELECT * FROM jugadores WHERE nacionalidad = 'argentino'`;
+   db.query(sql, (err, result) => {
+      if (err) {
+         console.error("Error al obtener jugadores argentinos:", err);
+         res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+         res.json(result);
+      }
+   });
+});
+
+app.get('/peso', (req, res) => {
+   const sql = `SELECT * FROM jugadores WHERE peso > 75 and peso < 80`;
+   db.query(sql, (err, result) => {
+      if (err) {
+         console.error("Error al obtener peso:", err);
+         res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+         res.json(result);
+      }
+   });
+});
+
+app.get('/altura', (req, res) => {
+   const sql = `SELECT * FROM jugadores ORDER BY estatura DESC LIMIT 1;`;
+   db.query(sql, (err, result) => {
+      if (err) {
+         console.error("Error al obtener jugador mas alto:", err);
+         res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+         res.json(result);
+      }
+   });
+});
+
+app.delete('/borrar', (req, res) => {
+   const { posicion } = req.body;
+   const sql = `DELETE FROM jugadores WHERE pos = ?`;
+   db.query(sql, [posicion], (err, result) => {
+     if (err) {
+       console.error(`Error al borrar jugadores en la posición ${posicion}:`, err);
+       res.status(500).json({ error: 'Error interno del servidor' });
+     } else {
+       res.json({ success: true });
+     }
+   });
+ });
+
+ app.get('/jugador-por-nombre/:nombre', (req, res) => {
+   const { nombre } = req.params;
+   const sql = `SELECT * FROM jugadores WHERE nombre = ?`;
+   db.query(sql, [nombre], (err, result) => {
+     if (err) {
+       console.error(`Error al obtener jugador por nombre ${nombre}:`, err);
+       res.status(500).json({ error: 'Error interno del servidor' });
+     } else {
+       res.json(result);
+     }
+   });
+ });
+ 
+ app.put('/modificar-por-nombre/:nombre', (req, res) => {
+   const { nombre } = req.params;
+   const { edad, estatura, nacionalidad, pos, peso } = req.body;
+ 
+   const sql = `
+     UPDATE jugadores
+     SET edad = ?, estatura = ?, nacionalidad = ?, pos = ?, peso = ?
+     WHERE nombre = ?`;
+ 
+   db.query(sql, [edad, estatura, nacionalidad, pos, peso, nombre], (err, result) => {
+     if (err) {
+       console.error(`Error al modificar jugador ${nombre}:`, err);
+       res.status(500).json({ error: 'Error interno del servidor' });
+     } else {
+       res.json({ success: true });
+     }
+   });
+ });
+
+   app.get('/', (req, res) => {
+      fs.readFile('index.html', 'utf8', (err, data) => {
+         if (err) {
+            console.error(err);
+            res.status(500).send('Error interno del servidor');
+         } else {
+            res.send(data);
+         }
+      });
+    });
+});
+
+
+app.listen(app.get('port'), ()=> {
+   console.log('servidor conectado!');
+});
+
